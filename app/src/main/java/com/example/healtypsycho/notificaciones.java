1d22101b80c97d4dc5e492c2cd6f7a2d40a5090a
jorge <jrobles9@ucol.mx>
2023-10-17 12:26:45 -0600

    codigo java de la vista( lis_doctores). creacion del archivo notificaciones.

---
+++ app/src/main/java/com/example/healtypsycho/notificaciones.java
@@ -0,0 +1,17 @@
+package com.example.healtypsycho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class notificaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificaciones);
+        
+    }
+}
