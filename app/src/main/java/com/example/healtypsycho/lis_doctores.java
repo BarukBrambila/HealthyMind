4f522b2b210ea69e368ec32512d1400c7ddd0c25
jorge <jrobles9@ucol.mx>
2023-10-17 10:12:05 -0600

    agregue la vista, lista de doctores donde se pueden consultar a los psicologos

---
+++ app/src/main/java/com/example/healtypsycho/lis_doctores.java
@@ -0,0 +1,14 @@
+package com.example.healtypsycho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lis_doctores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lis_doctores);
+    }
+}
