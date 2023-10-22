77bd1996b37498ca7744931e7094c3800245dc4f
jorge <jrobles9@ucol.mx>
2023-10-22 17:38:36 -0600

    se agrego la vista (Inicio paciente)

---
+++ app/src/main/java/com/example/healtypsycho/ini_paciente.java
@@ -0,0 +1,14 @@
+package com.example.healtypsycho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ini_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ini_paciente);
+    }
+}
