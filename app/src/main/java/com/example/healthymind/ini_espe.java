0668bad48362b0d03671ddce22a9681866a74faa
jorge <jrobles9@ucol.mx>
2023-10-26 12:24:04 -0600

    se agrego la vista (Inicio especialista).

--- app/src/main/java/com/example/healthymind/ini_espe.java
+++ app/src/main/java/com/example/healthymind/ini_espe.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.healthymind.R;
 
-public class ini_paciente extends AppCompatActivity {
+public class ini_espe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ini_paciente);
+        setContentView(R.layout.activity_ini_espe);
     }
 }
